999aaf0228326c10db84d7b6ef615927a3918227
benmucha <43706062+benmucha@users.noreply.github.com>
2023-09-22 14:59:50 -0400

    Add database connection
    Add in-memory H2 database connection.

---
+++ cooker/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        connect();
+        System.out.println("Hello world!");
+    }
+
+    /**
+     * Connect to a sample database
+     */
+    public static void connect() throws SQLException {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:h2:mem:test";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection to SQLite has been established.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
